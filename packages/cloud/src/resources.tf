8c6d3fc5c7396264b2e84b62c37ee5e72fc242e4
Vlad Volkov <vlad@hiberbee.com>
2023-12-26 22:10:16 +0200

    Terraform gce node pool config

--- packages/cloud/src/resources.tf
+++ packages/cloud/src/resources.tf
@@ -1,4 +1,15 @@
 resource "google_container_cluster" "production" {
-
   name = "vortexia"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "workers"
+  cluster    = google_container_cluster.production.id
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+  }
 }
